5f8074a05e9e07526c4d083fec9255f854cfbb26
kidult00 <sysulj@gmail.com>
2025-05-29 10:03:24 +0800

    Initial Commit

---
+++ spotlight/spotlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotlightApp.swift
+//  spotlight
+//
+//  Created by ByteDance on 2025/5/29.
+//
+
+import SwiftUI
+
+@main
+struct spotlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ spotlightTests/spotlightTests.swift
@@ -0,0 +1,17 @@
+//
+//  spotlightTests.swift
+//  spotlightTests
+//
+//  Created by ByteDance on 2025/5/29.
+//
+
+import Testing
+@testable import spotlight
+
+struct spotlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
